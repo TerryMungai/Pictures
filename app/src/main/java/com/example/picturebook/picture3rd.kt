9b1adf22498c0f12b14180f4e2d03f29805f9c2c
TerryMungai <terymungai@gmail.com>
2020-07-29 23:56:39 +0300

    my second picture

---
+++ app/src/main/java/com/example/picturebook/picture3rd.kt
@@ -0,0 +1,11 @@
+package com.example.picturebook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class picture3rd : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture3rd)
+    }
+}
